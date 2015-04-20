cdc7969e4cb61640e7926f7d9fb47d2534db7feb
karol <karol.jurczenia@gmail.com>
2015-04-20 23:18:16 +0200

    Bisekcja oraz pochodna

--- MN_4/MN_4/MN_4.cpp
+++ MN_4/MN_4/MN_4.cpp
@@ -10,25 +10,17 @@ double moja_funkcja(double x)
 	return wynik;
 }
 
+double moja_funkcja_pochodna(double x) // x(3x + 8)
+{										// (x*cos(x) - sin(x)) / x^2
+	double wynik = x*((3 * x) + 8);
+	//double wynik = ((x*cos(x)) - sin(x)) / x *x;
+	return wynik;
+}
+
 void bisekcja(double a, double b, int it_end, double eps)
 {
-	double err = eps + 1;
-	//double it_start = 0;
 	double c;
 	double x;
-
-	double** x_vext = new double*[it_end];
-	double** fx = new double*[it_end];
-	double** xdif = new double*[it_end];
-
-	int indeks;
-
-	for (int i = 0; i < it_end; i++)
-	{
-		x_vext[i] = new double[it_end];
-		fx[i] = new double[it_end];
-		xdif[i] = new double[it_end];
-	}
 
 	for (int i = 0; i < it_end; i++)
 	{
@@ -41,16 +33,17 @@ void bisekcja(double a, double b, int it_end, double eps)
 		cout << "f(x)" << i + 1 << " = " << moja_funkcja(x) << endl;
 		cout << endl;
 
-		//a = moja_funkcja(a);
-		//b = moja_funkcja(b);
-		//x = moja_funkcja(x);
-
 		if (moja_funkcja(a)*moja_funkcja(x) < 0)
 			b = x;
 		else
 			a = x;
 	}
 	
+
+}
+
+void styczne(int x0, int x1, double eps)
+{
 
 }
 
