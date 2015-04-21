74964c95808ea4f21b11da62344ed6ad53fbc3b5
karol <karol.jurczenia@gmail.com>
2015-04-21 22:49:55 +0200

    Zrozum

--- MN_4/MN_4/MN_4.cpp
+++ MN_4/MN_4/MN_4.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 #define przedzial 1
+double eps = 0.000001;
 
-double moja_funkcja(double x)
+double fun0(double x)
 {
 	double wynik = sin(x) / x;
 	//double wynik = (x*x*x) + (4 * x*x) - 10;
@@ -12,14 +13,19 @@ double moja_funkcja(double x)
 	return wynik;
 }
 
-double moja_funkcja_pochodna(double x) // x(3x + 8)
+double fun0_d(double x) // x(3x + 8)
 {										// (x*cos(x) - sin(x)) / x^2
-	double wynik = x*((3 * x) + 8);
-	//double wynik = ((x*cos(x)) - sin(x)) / x *x;
+	//double wynik = x*((3 * x) + 8);
+	double wynik = ((x*cos(x)) - sin(x)) / x *x;
 	return wynik;
 }
 
-void bisekcja(double a, double b, int it_end, double eps)
+double fun1(double x)
+{
+	return (x - 1)*(x - 2)*(x - 3)*(x - 4);
+}
+
+void bisekcja(double a, double b, int it_end)
 {
 	double c;
 	double x;
@@ -29,11 +35,9 @@ void bisekcja(double a, double b, int it_end, double eps)
 	bool jest_x = true;
 	int licznik = 0;
 	
-	//b = a + 0.1;
 	b = a + przedzial;
 	while (true)
 	{
-		//b = a + 2.0;
 		if (a >= temp_b || b > temp_b)
 			break;
 		for (int i = 0; i < it_end; i++)
@@ -41,18 +45,12 @@ void bisekcja(double a, double b, int it_end, double eps)
 			c = a + ((b - a) / 2);
 			x = c;
 
-			//cout << "a" << i + 1 << " = " << a << endl;
-			//cout << "b" << i + 1 << " = " << b << endl;
-			//cout << "x" << i + 1 << " = " << x << endl;
-			//cout << "f(x)" << i + 1 << " = " << moja_funkcja(x) << endl;
-			//cout << endl;
-
-			if (moja_funkcja(a)*moja_funkcja(x) < 0)
+			if (fun0(a)*fun0(x) < 0)
 			{
 				b = x;
 				jest_x = true;
 			}
-			else if (moja_funkcja(b)*moja_funkcja(x) < 0)
+			else if (fun0(b)*fun0(x) < 0)
 			{
 				a = x;
 				jest_x = true;
@@ -67,10 +65,6 @@ void bisekcja(double a, double b, int it_end, double eps)
 			cout << endl;
 		}
 		
-		//a = temp_a + (3.0*(double)j);
-		//a = x;
-		//b += 0.1;
-		//a = temp_a;
 		a = temp_a + (przedzial*(double)licznik);
 		b = a + przedzial;
 	}
@@ -80,14 +74,13 @@ void bisekcja(double a, double b, int it_end, double eps)
 void stycznych(double x, int iter)
 {
 	double x_n, y, dy;
-	double eps = 0.0005;
-	int iter_max = 20;
+	int iter_max = 200;
 	x_n = x - 1;
-	y = moja_funkcja(x);
+	y = fun0(x);
 	while (abs(x_n - x) > eps && abs(y) > eps)
 	{
 		//dy = (fun(x + eps) - y) / eps;
-		dy = moja_funkcja_pochodna(x);
+		dy = fun0_d(x);
 
 		if (abs(dy) < eps || iter++ >= iter_max)
 		{
@@ -97,11 +90,10 @@ void stycznych(double x, int iter)
 
 		x_n = x;
 		x = x - y / dy;
-		y = moja_funkcja(x);
-		cout << x << endl;
-
+		y = fun0(x);
 	}
-	cout << iter_max << endl;
+	cout << x << endl;
+	//cout << dy << endl;
 	return;
 }
 
@@ -109,17 +101,12 @@ int main()
 {
 	double a = 2.0;
 	double b = 10.0;
-	double eps = 0.0005;
+	//double eps = 0.0005;
 	int iter = 20;
 
-	//for (int i = 1; i <= 6; i++)
-	//{
-	//	
 	//bisekcja(a, b, iter, eps);
-	stycznych(1, 0);
-	//	a += 3;
-	//	b += 3;
-	//}
+	for (int i = 1; i <= 10; i++)
+		stycznych(i, 0);
 
 	system("pause");
 	return 0;
